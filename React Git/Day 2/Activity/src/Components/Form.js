67a33bc4bbcbe2660e274c5337980e0fc10d3697
Naveenya <115526605+Naveenya7@users.noreply.github.com>
2023-11-07 20:55:57 +0530

    Add files via upload

---
+++ React Git/Day 2/Activity/src/Components/Form.js
@@ -0,0 +1,23 @@
+import {Award} from 'lucide-react';
+export const Form = () => {
+    return (
+    <>
+    {/* <div className="myform"> */}
+    <form className="f1">
+    <input type="text" name="name" id="" placeholder="Name"
+    className="fx1"/>
+    <input type="email" name="" id="" placeholder="Email"
+    className="fx1"/>
+    <input type="number" name="" id="" placeholder="Phone"
+    className="fx1"/>
+    <input type="text" name="" id="" placeholder="Username"
+    className="fx1"/>
+    <input type="password" name="" id="" placeholder="Password"
+    className="fx1"/>
+    <button style={{backgroundColor:'violet'}}><Award/>Register</button>
+    </form>
+    {/* </div> */}
+    </>
+    )
+    }
+    export default Form;
